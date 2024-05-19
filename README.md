171f8d98553d414aba16cffd5c4d65a792e0ef7c
saripudin <arieptea@gmail.com>
2024-05-19 22:09:40 +0700

    https://github.com/ariep1993/uts_pengolahcitra.git~

---
+++ README.md
@@ -0,0 +1,21 @@
+Tugas project Web App untuk melengkapi tugas UTS
+
+Tugas 1
+merubah RGb menjadi HSV:
+
+![img](img/RGB%20to%20HSV.png)
+
+Tugas 2
+Histogram
+
+![img](img/Histogram.png)
+
+Tugas 3
+Brignest & Contras
+
+![img](img/brignest%20%26%20Contras.png)
+
+Tugas 4
+Contour
+
+![img](img/contur.png)
---
+++ home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title('Profile')
+st.subheader('Nama : saripudin')
+st.subheader('NIM : 312210077')
+st.subheader('Kelas : TI.22.B1')
+st.subheader('Prodi : Teknik Infomatika')
+
+st.image("./img/saripudin1.jpeg", caption="saripudin", use_column_width="Always", width=200)
+
+
+st.link_button("Github", "https://github.com/ariep1993")
